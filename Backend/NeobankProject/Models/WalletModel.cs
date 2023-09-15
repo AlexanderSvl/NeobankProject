dd30228e876efbc2d6335fb43d8b114e0db8328e
AlexanderSvl <aleksander.svilarovv@gmail.com>
2023-09-15 23:41:38 +0300

    added wallet model

--- Backend/NeobankProject/Models/WalletModel.cs
+++ Backend/NeobankProject/Models/WalletModel.cs
@@ -1,6 +1,19 @@
-﻿namespace NeobankProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NeobankProject.Models
 {
     public class WalletModel
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public UserModel User { get; set; }
+
+        [Required]
+        public decimal Value { get; set; }
+
+        [Required]
+        public string DefaultCurrencyCode { get; set; }
     }
 }
