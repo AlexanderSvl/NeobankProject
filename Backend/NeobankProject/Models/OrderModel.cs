0dd0a8a3b91fa83bf17d5fb26cc7ea46f148b124
AlexanderSvl <aleksander.svilarovv@gmail.com>
2023-09-18 20:48:16 +0300

    Added order model

---
+++ Backend/NeobankProject/Models/OrderModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NeobankProject.Models
+{
+    public class OrderModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string OrderType { get; set; }
+
+        [Required]
+        public UserModel User { get; set; }
+
+        [Required]
+        public StockModel Stock { get; set; }
+
+        [Required]
+        public DateTime DateOfExecution { get; set; }
+    }
+}
