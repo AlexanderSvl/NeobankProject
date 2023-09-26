b2919e6b005bda5f41276822d7dff869d613f9f3
AlexanderSvl <aleksander.svilarovv@gmail.com>
2023-09-27 00:45:16 +0300

    uiuiuiu

--- Backend/NeobankProject/Program.cs
+++ Backend/NeobankProject/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
 builder.Services.AddScoped<ITradeRepository, TradeRepository>();
 builder.Services.AddScoped<IWalletRepository, WalletRepository>();
 
