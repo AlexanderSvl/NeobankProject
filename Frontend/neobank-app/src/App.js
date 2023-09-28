d8521aff99d82a83f99f6d593754615375a18797
AlexanderSvl <aleksander.svilarovv@gmail.com>
2023-09-28 21:39:28 +0300

    Added landing page

--- Frontend/neobank-app/src/App.js
+++ Frontend/neobank-app/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import logo from '../src/images/logo.png';  
+import '../src/index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="container">
+          <img className="image" src={logo} alt=""></img>
+
+          <div className="middle">
+            <div className="text">Join us!</div>
+          </div>
+        </div>
+
+        <h3 className="head1">Welcome to Neobank. </h3>
+        <h3 className="head2">Your best finance management system.</h3>
       </header>
     </div>
   );
