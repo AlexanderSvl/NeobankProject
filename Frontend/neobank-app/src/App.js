91e85a1967411e15458d9277b17884941e5d03c9
AlexanderSvl <aleksander.svilarovv@gmail.com>
2023-10-04 00:14:47 +0300

    Added menu in home page with all the pages

--- Frontend/neobank-app/src/App.js
+++ Frontend/neobank-app/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import LandingPageComponent from './components/LandingPage';
 import RegisterComponent from '../src/components/Register';
 import LoginComponent from './components/Login';
+import HomePageComponent from './components/homePage';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path={'/register'} element={< RegisterComponent />}></Route>
                 <Route path={'/login'} element={< LoginComponent />}></Route>
+                <Route path={'/home'} element={< HomePageComponent />}></Route>
                 <Route path={'/'} element={< LandingPageComponent />}></Route>
             </Routes>
         </Router>
