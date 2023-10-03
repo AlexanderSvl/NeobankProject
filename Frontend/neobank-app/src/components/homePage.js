9ae2ccd798be3b946d3e79e729a557d725933a29
AlexanderSvl <aleksander.svilarovv@gmail.com>
2023-10-04 00:38:49 +0300

    Added menu component

--- Frontend/neobank-app/src/components/homePage.js
+++ Frontend/neobank-app/src/components/homePage.js
@@ -1,62 +1,14 @@
 import React, {useState} from "react";
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-import '../styles/home-page.css';
 import logo from '../images/logo.png';
 import logoNoText from '../images/neobank-logo-notext.png';
 import TypingAnimation from "react-typed";
+import MenuComponent from "./menu";
 
 function HomePageComponent() {
     return (
         <div className="main-container">
-            <div className="menu">
-                <div className="menu-container">
-                    <img className="logo" src={logoNoText}></img>
-
-                    <p
-                        className="menuItem"
-                        style={{
-                        marginTop: "8vh"
-                    }}>My Wallet</p>
-                    <p
-                        className="menuItem"
-                        style={{
-                        marginTop: "2vh"
-                    }}>Transactions</p>
-
-                    <p
-                        className="menuItem"
-                        style={{
-                        marginTop: "10vh"
-                    }}>Investing</p>
-                    <p
-                        className="menuItem"
-                        style={{
-                        marginTop: "2vh"
-                    }}>Stocks</p>
-                    <p
-                        className="menuItem"
-                        style={{
-                        marginTop: "2vh"
-                    }}>Crypto</p>
-
-                    <p
-                        className="menuItem"
-                        style={{
-                        marginTop: "10vh"
-                    }}>Spendings</p>
-                    <p
-                        className="menuItem"
-                        style={{
-                        marginTop: "2vh"
-                    }}>Reports</p>
-
-                    <p
-                        className="menuItem"
-                        style={{
-                        marginTop: "9vh"
-                    }}>Settings</p>
-                </div>
-            </div>
+            <MenuComponent></MenuComponent>
             <div className="main"></div>
         </div>
     )
---
+++ Frontend/neobank-app/src/components/menu.js
@@ -0,0 +1,61 @@
+import React, {useState} from "react";
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import '../styles/menu.css';
+import logo from '../images/logo.png';
+import logoNoText from '../images/neobank-logo-notext.png';
+
+function MenuComponent() {
+    return (
+        <div className="menu">
+            <div className="menu-container">
+                <img className="logo" src={logoNoText}></img>
+
+                <p
+                    className="menuItem"
+                    style={{
+                    marginTop: "6vh"
+                }}>My Wallet</p>
+                <p
+                    className="menuItem"
+                    style={{
+                    marginTop: "2vh"
+                }}>Transactions</p>
+
+                <p
+                    className="menuItem"
+                    style={{
+                    marginTop: "10vh"
+                }}>Investing</p>
+                <p
+                    className="menuItem"
+                    style={{
+                    marginTop: "2vh"
+                }}>Stocks</p>
+                <p
+                    className="menuItem"
+                    style={{
+                    marginTop: "2vh"
+                }}>Crypto</p>
+
+                <p
+                    className="menuItem"
+                    style={{
+                    marginTop: "10vh"
+                }}>Spendings</p>
+                <p
+                    className="menuItem"
+                    style={{
+                    marginTop: "2vh"
+                }}>Reports</p>
+
+                <p
+                    className="menuItem"
+                    style={{
+                    marginTop: "10vh"
+                }}>Settings</p>
+            </div>
+        </div>
+    )
+}
+
+export default MenuComponent;
