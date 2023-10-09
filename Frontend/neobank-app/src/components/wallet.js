ecd86c1d974431e95e1ebc601294c2aeec34d9ed
AlexanderSvl <aleksander.svilarovv@gmail.com>
2023-10-09 15:49:38 +0300

    Added wallet ballance animation

--- Frontend/neobank-app/src/components/wallet.js
+++ Frontend/neobank-app/src/components/wallet.js
@@ -3,12 +3,33 @@ import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import TypingAnimation from "react-typed";
 import '../styles/wallet.css';
 import wallet from '../images/wallet-2.png'
+import {useSpring, animated} from "react-spring";
 
-function WalletComponent(){
+function WalletComponent() {
+    let balance = 674.50;
+
     return (
         <div className="wallet">
+            <h1 className="balance">{AnimateNumber(balance)}</h1>
         </div>
     )
+}
+
+function AnimateNumber(n) {
+    const {number} = useSpring({
+        from: {
+            number: 0.00
+        },
+        number: n,
+        delay: 200,
+        config: {
+            mass: 1,
+            tension: 20,
+            friction: 10
+        }
+    });
+
+    return <animated.div>{number.to((n) => `$${n.toFixed(2)}`)}</animated.div>;
 }
 
 export default WalletComponent;
